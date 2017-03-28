d3a76645af1d0ab5b9b63a4681495a02ec4c6cbf
Tiago Melo <tiagotele@gmail.com>
2017-03-28 18:59:12 -0300

    Removing unecessary files. Renaming necessary files.

--- sample_filter.py
+++
@@ -1,6 +0,0 @@
-from graph_tool.all import *
-from numpy.random import *  # for random sampling
-
-g, pos = triangulation(random((500, 2)) * 4, type="delaunay")
-tree = min_spanning_tree(g)
-graph_draw(g, pos=pos, edge_color=tree, output="min_tree.pdf")
--- sample_filtered.py
+++
@@ -1,7 +0,0 @@
-from graph_tool.all import *
-from numpy.random import *  # for random sampling
-
-g, pos = triangulation(random((500, 2)) * 4, type="delaunay")
-tree = min_spanning_tree(g)
-g.set_edge_filter(tree)
-graph_draw(g, pos=pos, output="min_tree_filtered.pdf")
