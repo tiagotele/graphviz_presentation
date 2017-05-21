1fdc4d664c45f372cbd4403a8d053fd479fadba6
Tiago Melo <tiagotele@gmail.com>
2017-05-21 19:07:44 -0300

    Construção das matrizes do algoritmo do método do grafo bipartido.

--- metodo_grafo_bipartido/algoritmo.py
+++ metodo_grafo_bipartido/algoritmo.py
@@ -4,6 +4,7 @@
 import csv
 import os
 import copy
+import operator
 
 #Funções auxiliares
 def load_csv(nome_do_arquivo, array_destino):
@@ -18,36 +19,41 @@ def matriz_de_custo_associado(nome_do_arquivo):
     matriz = []
     load_csv(nome_do_arquivo, matriz)
     matriz_copia = copy.copy(matriz)
-    for indice_linha in range(len(matriz)):
+    for indice_linha in range(1,len(matriz)):
         custo_acumulado = 0
-        for indice_item in range(len(matriz[indice_linha])):
+        for indice_item in range(1,len(matriz[indice_linha])):
             custo_acumulado += int(matriz[indice_linha][indice_item])
-        print ("Custo acumulado" , custo_acumulado)
         matriz_copia[indice_linha].append(custo_acumulado)
     return matriz_copia
 
+def sort_table(table, col=0):
+    return sorted(table, key=operator.itemgetter(col))
+
+def ordena_matriz_de_custo_associado(matriz_de_custo_associado):
+    return sort_table(matriz_de_custo_associado,len(matriz_de_custo_associado))
+
+def constroi_tabela_de_nos_numerados(matriz):
+    matriz_de_nos_numerados = []
+    for indice_da_matriz in range(0,len(matriz)-1):
+        linha_de_matriz_de_no_numerado = []
+        linha_de_matriz_de_no_numerado.append(matriz[indice_da_matriz][0])
+        linha_de_matriz_de_no_numerado.append(matriz[indice_da_matriz][len(matriz[indice_da_matriz])-1])
+        linha_de_matriz_de_no_numerado.append(indice_da_matriz+1)
+        print("linha_de_matriz_de_no_numerado" , linha_de_matriz_de_no_numerado)
+        matriz_de_nos_numerados.append(linha_de_matriz_de_no_numerado)
+        print("matriz_de_nos_numerados" , matriz_de_nos_numerados)
+    return matriz_de_nos_numerados
+
 os.system('clear')
 
-#def adiciona_numero_do_no_ordenado():
 
-def numeracao_de_nos(matriz_de_custo):
-    matriz_numeracao_de_no = []
-    linha_numeracao_de_no = []
-    for indice_linha in range(len(matriz_de_custo)):
-        linha_numeracao_de_no.append(indice_linha)
-        for indice_item in range(len(matriz_de_custo[indice_linha])):
-            linha_numeracao_de_no.append(matriz_de_custo[indice_linha][len(matriz_de_custo)])
-        matriz_numeracao_de_no.append(linha_numeracao_de_no)
-
-
-
-#Carrega n nós da rede
-
-#Carrega matriz custo associado com cada nó
-#matriz_de_custo = carrega_matriz_de_custo()
-
-#Carrega k conectividade
-
-
-matriz_de_custo=matriz_de_custo_associado('custo_associado.csv')
+matriz_de_custo = matriz_de_custo_associado('custo_associado.csv')
+print ("Matriz de custo")
 print(matriz_de_custo)
+print ("Matriz de custo ordenada")
+matriz_de_custo_ordenada = ordena_matriz_de_custo_associado(matriz_de_custo)
+print(matriz_de_custo_ordenada)
+print("Tabela de nós numerados")
+tabela_de_nos_numerados = []
+tabela_de_nos_numerados = constroi_tabela_de_nos_numerados(matriz_de_custo_ordenada)
+print(tabela_de_nos_numerados)
